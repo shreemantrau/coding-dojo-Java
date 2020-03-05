4e525c0593649fe57bda9a54fc9351bea9905477
shreemantrau <49795327+shreemantrau@users.noreply.github.com>
2020-03-05 15:29:12 -0800

    Add files via upload

---
+++ Fundamentals/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,20 @@
+class FizzBuzz {
+    public String fizzBuzz(int x) {
+        if (x == 3) {
+            return "Fizz";
+        } else if (x == 5) {
+            return "Buzz";
+        } else if (x == 15) {
+            return "FizzBuzz";
+        }
+
+        else if (x == 2) {
+            return "2";
+        }
+
+        else {
+            return "I honestly dont know what to return and what you want!";
+        }
+
+    }
+}
---
+++ Fundamentals/FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,7 @@
+class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        String s = fb.fizzBuzz(1);
+        System.out.print(s);
+    }
+}
