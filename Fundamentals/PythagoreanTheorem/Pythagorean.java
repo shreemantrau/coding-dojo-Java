d960cfcdc56659b074ef2326f3761acca1c226bd
shreemantrau <49795327+shreemantrau@users.noreply.github.com>
2020-03-05 15:35:51 -0800

    Add files via upload

---
+++ Fundamentals/PythagoreanTheorem/Pythagorean.java
@@ -0,0 +1,12 @@
+class Pythagorean{
+    
+    public double calcHypo(int a, int b){
+        // double c=0;
+        double fi=a;
+        double si=b;
+        double c=Math.sqrt((fi*fi) + (si*si));
+        //System.out.println(c);
+        return c;
+    }
+    
+}
---
+++ Fundamentals/PythagoreanTheorem/PythagoreanTest.java
@@ -0,0 +1,7 @@
+class PythogoreanTest{
+    public static void main(String [] args){
+        Pythagorean p= new Pythagorean();
+        double hypo= p.calcHypo(6,8);
+        System.out.println(hypo);
+    }
+}
